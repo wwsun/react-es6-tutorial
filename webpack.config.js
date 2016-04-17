3135fb1373abcedb2f001d12a59509151cbad113
Weiwei SUN <ww.sun@outlook.com>
2016-04-17 16:08:39 +0800

    update babel-loder config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = webpackConfig => {
 
   webpackConfig.module.loaders.unshift(
     {
-      test: /src(\\|\/).+\.jsx?$/,
+      test: /\.jsx?/,
       exclude: /node_modules/,
       loader: 'babel',
       query: { presets: [ 'es2015', 'react', 'stage-0' ] }
