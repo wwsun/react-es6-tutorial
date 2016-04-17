f59c81a105135b4d20513fd2f3a4234841175713
Weiwei SUN <ww.sun@outlook.com>
2016-04-17 14:57:42 +0800

    refactor: remove all old files

--- src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './components/root.jsx';
-
-let attachElement = document.getElementById('react-app');
-
-ReactDOM.render(<Root />, attachElement);
