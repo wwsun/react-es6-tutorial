99e38b11c6f051646a8d48b641489d52aae340f0
Weiwei SUN <ww.sun@outlook.com>
2016-04-30 14:18:22 +0800

    feat: add sass support

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = webpackConfig => {
       exclude: /node_modules/,
       loader: 'babel',
       query: { presets: [ 'es2015', 'react', 'stage-0' ] }
+    },
+    {
+      test: /\.scss?/,
+      loader: ['style', 'css', 'sass']
     }
   );
 
