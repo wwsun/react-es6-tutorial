208707ebc488a697580b65770863a015849c7e24
Weiwei SUN <ww.sun@outlook.com>
2016-05-15 17:04:58 +0800

    feat: add sass support

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = webpackConfig => {
+  webpackConfig.babel.plugins.push('antd');
+
+  webpackConfig.module.loaders.unshift(
+    {
+      test: /\.jsx?/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: { presets: ['es2015', 'react', 'stage-0'] },
+    },
+    {
+      test: /\.scss?/,
+      loaders: ['style', 'css', 'sass'],
+    }
+  );
+
+  return webpackConfig;
+};
